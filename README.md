73a28d0b2a3c6eba5337467213a7e449e58cb5c9
Michał Kurzeja <mike.kurzeja@gmail.com>
2018-03-02 23:38:56 +0100

    Added README

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# go-messenger
+# review-tools
+
+## Introduction
+This is a simple tool that collects label information from Github Pull Requests. In the future it may become something more, hence a more generic name... :)
+
+## Installation
+1. Clone the repository
+2. Run `dep ensure`
+3. Run `go install`
+
+Make sure to have your `$GOPATH` added as part of `$PATH`!!!
+
+## How to run
+### Basic
+```
+$ review-tools label-stats -o golang -r go
+```
+
+### Private repositories
+```
+$ review-tools label-stats -o golang -r go -t <OAuth Token>
+```
+
+### Help
+```
+$ review-tools                  # Basic help
+$ review-tools -h               # The same as above
+$ review-tools label-stats -h   # Sub-command help
+```
+
+## Env
+You can omit passing the options into the command by defining env variables. Please refer to the in-command help to identify the right env variables.
--- tokenstats/label_token_collector.go
+++ tokenstats/label_token_collector.go
@@ -2,6 +2,7 @@ package tokenstats
 
 import (
 	"context"
+	"net/http"
 
 	"github.com/google/go-github/github"
 	"github.com/urfave/cli"
@@ -13,18 +14,6 @@ type LabelTokenCollector struct {
 	cliCtx *cli.Context
 }
 
-func NewLabelTokenCollector(ctx context.Context, cliCtx *cli.Context) *LabelTokenCollector {
-	tokenSource := oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: cliCtx.String("token")},
-	)
-	oauthClient := oauth2.NewClient(ctx, tokenSource)
-
-	return &LabelTokenCollector{
-		client: github.NewClient(oauthClient),
-		cliCtx: cliCtx,
-	}
-}
-
 func (c *LabelTokenCollector) Collect(ctx context.Context) TokenStats {
 	issues, _, err := c.client.Issues.ListByRepo(ctx, c.cliCtx.String("owner"), c.cliCtx.String("repository"), &github.IssueListByRepoOptions{})
 
@@ -32,6 +21,33 @@ func (c *LabelTokenCollector) Collect(ctx context.Context) TokenStats {
 		panic(err)
 	}
 
+	return extractStats(issues)
+}
+
+func NewLabelTokenCollector(ctx context.Context, cliCtx *cli.Context) *LabelTokenCollector {
+	token := cliCtx.String("token")
+	httpClient := getHttpClient(token, ctx)
+
+	return &LabelTokenCollector{
+		client: github.NewClient(httpClient),
+		cliCtx: cliCtx,
+	}
+}
+
+func getHttpClient(token string, ctx context.Context) *http.Client {
+	var oauthClient *http.Client
+
+	if len(token) > 0 {
+		tokenSource := oauth2.StaticTokenSource(
+			&oauth2.Token{AccessToken: token},
+		)
+		oauthClient = oauth2.NewClient(ctx, tokenSource)
+	}
+
+	return oauthClient
+}
+
+func extractStats(issues []*github.Issue) TokenStats {
 	stats := make(map[string]map[string]int)
 
 	for _, issue := range issues {
